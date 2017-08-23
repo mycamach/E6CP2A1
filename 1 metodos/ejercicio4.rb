f1d4d582662da9e2e0b114187d01a112eb3974ca
mycamach <manoli.camacho.a@gmail.com>
2017-08-23 20:56:56 -0300

    4 ready

--- 1 metodos/ejercicio4.rb
+++ 1 metodos/ejercicio4.rb
@@ -1,3 +1,10 @@
 # Crear un método que muestre en pantalla un saludo, el método
 # debe recibir un parámetro, si ese parámetro es el string "Hola" el
 # método debe mostrar en pantalla "Hola Mundo".
+
+def greeting(string)
+  return "#{string} Mundo" if string == 'Hola'
+  "Hola #{string}"
+end
+
+puts greeting('Hola')
