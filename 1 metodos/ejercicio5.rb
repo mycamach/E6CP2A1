e0e0fd25fd7257d317811627c517b646417a2e53
mycamach <manoli.camacho.a@gmail.com>
2017-08-24 10:50:03 -0300

    5 ready with Rubocop changes applied

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,9 @@
 # Crear un método que reciba como parámetro dos números enteros positivos
 # e imprima los números pares que existen entre esos dos números.
+
+def check_even?(a, b)
+  (a..b).each { |i| puts i if i.even? }
+  (b..a).each { |i| puts i if i.even? }
+end
+
+check_even?(5, 12)
