d987173244ba027179391fd77d02c51b4d6eb99e
mycamach <manoli.camacho.a@gmail.com>
2017-08-30 13:11:55 -0300

    2 ready

--- 3 strings/ejercicio2.rb
+++ 3 strings/ejercicio2.rb
@@ -4,18 +4,26 @@
 # 3. Crear un método que devuelva un arreglo con la cantidad de caracteres que tiene cada nombre.
 
 desafio_G9 = %w[Erick Francisco Cristian Richard Luis Sofia Camila Catalina
-Exequiel Fernanda Andres Jocelin David Miguel Javier Gricel]
+                Exequiel Fernanda Andres Jocelin David Miguel Javier Gricel]
 
 print desafio_G9, "\n"
 
-desafio_G9.map { |nombre| print "#{nombre} " if nombre.length > 5 }
+print "###################\n"
 
-desafio_G9_downcase = desafio_G9.map { |nombre| nombre.downcase }
+desafio_G9.map { |n| print "#{n} " if n.length > 5 }
 
-print desafio_G9_downcase
+print "####################\n"
 
-def char_length (array, nombre)
-char_length_array = array.nombre.length	
+desafio_G9_d = desafio_G9.map { |nombre| nombre.downcase }
+# Preguntar por el comentario de Rubocop
+
+print desafio_G9_d, "\n"
+print "####################\n"
+
+def array_str_length(array)
+  a = []
+  array.each { |i| a << i.length }
+  print a
 end
 
-char_length(desafio_G9)
+array_str_length(desafio_G9)
