aa4ca0f67b7297339fcc57ba58c9fb3e0ff7df83
mycamach <manoli.camacho.a@gmail.com>
2017-08-30 11:56:50 -0300

    Added some changes

--- 3 strings/_ejercicioX.rb
+++ 3 strings/_ejercicioX.rb
@@ -2,14 +2,16 @@
 
 song = 'la mar estaba serena, serena estaba la mar'
 
-# Se pide mostrar el string dos veces de forma nomal, luego
+# Se pide mostrar el string dos veces de forma normal, luego
 # cambiando las todas las vocales por la vocal a, luego por e
 # seguido por el resto de las vocales
+
+puts "#{song}\n" * 2
 
 vowels = %w(a e i o u)
 vowels.each do |vowel|
   song.gsub!(/[aeiou]/, vowel)
   #Descomentar línea 13 en OSx
-  `say "#{song}" -v diego`
+  'say "#{song}" -v diego'
   p song
 end
